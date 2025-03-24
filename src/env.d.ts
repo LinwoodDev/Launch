0389341fbc483b87d9ceb9cf022ca5e60cb009e5
CodeDoctorDE <codedoctor@linwood.dev>
2025-03-24 13:00:34 +0100

    Add deploy, improve page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly APP_NAME?: string;
+  readonly APP_LAUNCH_URL?: string;
+  readonly APP_DOWNLOAD_URL: string;
+  readonly APP_WEB_URL?: string;
+  readonly APP_WEB_PREVIEW_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
